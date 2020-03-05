e2b10104bdabfcdb52d7609d54cdddc5c37a8631
Пукась Михаил Андреевич <pumi@ya.ru>
2020-03-05 20:12:09 +0300

    book img

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,23 @@
 <?php
+
+?>
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+    <title>Веб справочник мессии</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Astrological portal"/>
+    <meta name="theme-color" content="#9f70ed">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+<img src="images/book.jpg" class="book center">
+
+</body>
+</html>
